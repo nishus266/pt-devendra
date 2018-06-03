56aaa34926347a1ac598d64cee47b28920667fc0
nishus266 <30533716+nishus266@users.noreply.github.com>
2018-06-03 09:30:11 +0530

    1st

---
+++ lyrics.php
@@ -0,0 +1,61 @@
+<!doctype html>
+
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <meta name="description" content="singing portal">
+
+    <meta name="author" content="devandra kaushik">
+
+    <link rel="icon" href="img/praan.png">
+
+
+    <title>Pt Devendra Kaushik</title>
+
+
+    <!-- CSS -->
+
+    
+<link href="css/main.css" rel="stylesheet">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    
+<link href="css/mobile.css" rel="stylesheet">
+
+        
+  </head>
+
+
+
+  <body class="g1" id="g1">
+  <section id="mainhead" class="mainhead">
+   <span class="logo"><img src="img/logo.png" width="200px" height="150px" ></span>
+</section>
+  <section class="head" id="head">
+            <span>Home</span>
+            <span>About</span>
+            <span>Contact</span>
+  </section>
+<div style="color:#d9edf7;">
+
+<h3>Songs By Pt Devendra Kaushik </h3>
+
+<ol>
+  <li><a href="https://drive.google.com/file/d/1pZqsTse9EPed0oLRIWW0AXrrDSiXZJwE/view?usp=drive_web" style="color:#d9edf7;"> Panchakchhar Namahshivaya ? Pt.Devendra Kaushik.mp3</a></li>
+
+  <li><a href="https://drive.google.com/file/d/1Rh3Swi7d-9GQAtT2vk_f8oTLOF0gBtcO/view?usp=drive_web" style="color:#d9edf7;"> Ankhiyan Haridarshan Ki Pyasi ? Aabhas.mp3</a></li>
+
+  <li><a href="https://drive.google.com/file/d/14l6lSbK31l0B9Pn6GS_S-QC1W17_zUAd/view?usp=drive_web" style="color:#d9edf7;"> Duniya Darshan Ka Hae Mela ? Aabhas.mp3</a></li>
+</ol>
+
+</div>
+
+
+  <script src="script/main.js"></script>
+  </body>
+  </html>
---
+++ script/main.js
@@ -0,0 +1,8 @@
+setInterval(function(){
+document.getElementById("img").className="img1";
+setInterval(function(){
+document.getElementById("img").className="img2";
+},5000);
+},5000);
+
+ 
